d783307cdcbf6dd1389cde33a4d6026f7904b766
anierajjs <salaysayanierajoy.bsit@gmail.com>
2024-02-24 21:54:56 +0800

    modified ce function

--- js/script.js
+++ js/script.js
@@ -4,15 +4,32 @@ console.log(actions);
 console.log(ans);
 let expression = '';
 let a = 0;
+
 actions.addEventListener('click', (e) => {
     console.log(e.target);
     const value = e.target.dataset['value'];
 
+    // if (value !== undefined) {
+    //     if (value == 'ce') {
+    //         // removing the last entered character
+    //         expression = expression.toString().slice(0, -1); // Convert to string before using slice
+    //         ans.value = expression || 0; // Set to 0 if expression is empty
+    //         return true;
     if (value !== undefined) {
         if (value == 'ce') {
+            // Clear the entire number entered
             expression = '';
             ans.value = 0;
             return true;
+            
+        } else if (value == 'c') {
+            // Function to clear the entire expression
+            expression = '';
+            ans.value = 0;
+        } else if (value == '=') {
+            const answer = eval(expression);
+            expression = answer;
+            ans.value = expression;
         } else if (value == 'x^2') {
             expression = square();
         } else if (value == 'radic') {
@@ -25,32 +42,14 @@ actions.addEventListener('click', (e) => {
             expression = Math.cos(expression);
         } else if (value == 'tan') {
             expression = Math.tan(expression);
-        } else if (value == 'c') {
-			clearLastInput();		
-        } else if (value == '=') {
-            const answer = eval(expression);
-            expression = answer;
-
         } else {
             expression += value;
-        }
-
-        if (expression == undefined) {
-            expression = '';
-            ans.value = 0;
-        } else {
             ans.value = expression;
         }
-        // expression += value;
     }
-
+    
 });
 
 const square = () => {
     return eval(expression * expression);
-}
-
-const clearLastInput = () => {
-    expression = expression.slice(0, -1);
-    ans.value = expression;
 }
