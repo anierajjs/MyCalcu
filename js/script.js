4231a3b081839d55a6a0aa16f73af26637635345
anierajjs <salaysayanierajoy.bsit@gmail.com>
2024-02-26 10:37:53 +0800

    updated overall logic and ui

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ const ans = document.querySelector('.ans');
 console.log(actions);
 console.log(ans);
 let expression = '';
+let lastEntry = '';
 let a = 0;
+ans.value = 0;
 
 actions.addEventListener('click', (e) => {
     console.log(e.target);
@@ -17,9 +19,15 @@ actions.addEventListener('click', (e) => {
     //         return true;
     if (value !== undefined) {
         if (value == 'ce') {
-            // Clear the entire number entered
-            expression = '';
-            ans.value = 0;
+            if (lastEntry == '=') {
+                expression = '';
+                ans.value = 0;
+            }
+            else {
+                // Clear the last entry input
+                expression = expression.toString().slice(0, -1);
+                ans.value = expression || 0; 
+            }
             return true;
             
         } else if (value == 'c') {
@@ -32,24 +40,28 @@ actions.addEventListener('click', (e) => {
             ans.value = expression;
         } else if (value == 'x^2') {
             expression = square();
-        } else if (value == 'radic') {
-            expression = Math.sqrt(expression);
-        } else if (value == 'log') {
-            expression = Math.log(expression);
-        } else if (value == 'sin') {
-            expression = Math.sin(expression);
-        } else if (value == 'cos') {
-            expression = Math.cos(expression);
-        } else if (value == 'tan') {
-            expression = Math.tan(expression);
-        } else {
-            expression += value;
-            ans.value = expression;
+        } else {    
+            if (lastEntry == '=' && isNumeric(value)) {
+                expression = value;
+                ans.value = expression;
+            }
+            else {
+                expression += value;
+                ans.value = expression;
+            }
         }
+        lastEntry = value;
     }
     
 });
 
+function isNumeric(str) {
+    if (typeof str != "string") return false // we only process strings!  
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+           !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+  }
+  
+  
 const square = () => {
     return eval(expression * expression);
 }
